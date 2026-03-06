ba70cdc1784f43a6a75bc6642d4c112eb9cd07b9
PHUNG MINH NGHIA <nghiapmhe182314@fpt.edu.vn>
2026-03-06 22:42:10 +0700

    Fix login OAuth2 Google

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,10 +21,10 @@ const LoginPage = () => {
 
     try {
       const userData = await login({ email, password });
-      
+
       // Get user role from userData and redirect accordingly
       const userRole = userData?.role?.toUpperCase();
-      
+
       if (userRole === "TEACHER") {
         navigate("/teacher/classrooms");
       } else if (userRole === "STUDENT") {
@@ -190,7 +190,7 @@ const LoginPage = () => {
 
               <div className="social-login">
                 <a
-                  href="http://localhost:8080/oauth2/authorization/google"
+                  href="http://localhost:8081/oauth2/authorization/google"
                   className="social-button google"
                 >
                   <svg viewBox="0 0 24 24" width="20" height="20">
